2a46da157959f322885c4c8ffb3a6993fd14469f
Dion Munipi <dion.munipi@kcl.ac.uk>
2022-09-28 12:39:40 +0100

    Write median function

--- median.py
+++ median.py
@@ -1,10 +1,22 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def getMedian(list):
+    sortedList = sorted(list)
+    length = len(list)
+    index = (length - 1) // 2
+
+    if length % 2 == 0:
+        return sortedList[index] + sortedList[index + 1]) / 2.0
+    else:
+        return sortedList[index]
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        getMedian(numbers)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
